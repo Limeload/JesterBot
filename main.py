f62c71c0de1773f115015f7d00ce3575ec0e50d3
Shraddha Rao <shraddharaom@gmail.com>
2023-04-06 19:46:28 -0500

    modified

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-# Import necessary libraries
 import random
+
+# Define global variables to store the chat history and the user's name
+chat_history = []
+user_name = ""
 
 # Define your main function
 def main():
-    # Print a welcome message
+    # Print a welcome message and ask for the user's name
+    global user_name
     print("Welcome to my chatbot!")
+    user_name = input("What's your name? ")
 
     # Start a loop to keep the chatbot running
     while True:
         # Get user input
-        user_input = input("You: ")
+        user_input = input(f"{user_name}: ")
 
-        # Generate a response
-        bot_response = generate_response(user_input)
+        # Add the user's input to the chat history
+        chat_history.append(user_input)
+
+        # Generate a response based on the chat history
+        bot_response = generate_response(chat_history)
+
+        # Add the bot's response to the chat history
+        chat_history.append(bot_response)
 
         # Print the bot's response
-        print("Bot: " + bot_response)
+        print(f"Bot: {bot_response}")
 
         # Check if the user wants to quit
         if user_input.lower() == "quit":
@@ -23,7 +34,7 @@ def main():
             break
 
 # Define a function to generate the bot's response
-def generate_response(user_input):
+def generate_response(chat_history):
     # Define a list of possible responses
     responses = ["I'm not sure what you mean.",
                  "Can you please rephrase that?",
@@ -33,7 +44,21 @@ def generate_response(user_input):
                  "I'm sorry, I don't understand.",
                  "Let's talk about something else."]
 
-    # Return a randomly selected response
+    # Check if this is the first interaction with the user
+    if len(chat_history) == 1:
+        return f"Nice to meet you, {user_name}! How can I help you today?"
+
+    # Check if the user has asked a question in the previous message
+    if "?" in chat_history[-2]:
+        return "I'm not sure, what do you think?"
+
+    # Check if the user has mentioned a topic in the previous message
+    keywords = ["weather", "sports", "movies", "music", "food"]
+    for word in chat_history[-2].split():
+        if word.lower() in keywords:
+            return f"I also like {word.lower()}! What's your favorite {word.lower()}?"
+
+    # If none of the above conditions are met, return a random response
     return random.choice(responses)
 
 # Call the main function
